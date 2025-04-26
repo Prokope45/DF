ab30f54ac6a679b67d2b970ae316ba0aff9186d1
Jay <52184578+Prokope45@users.noreply.github.com>
2025-04-26 11:26:01 -0500

    Update main code

--- process/main.py
+++ process/main.py
@@ -8,6 +8,12 @@ leases_df = pd.read_csv("../data/Leases.csv")
 # Omit rows with na company_name
 leases_df = leases_df[leases_df["company_name"].notna()]
 
+# Drop na rows for company name
+# leases_df = pd.DataFrame(leases_df, columns=["company_name"]).dropna()
+
+# Omit rows with na company_name
+leases_df = leases_df[leases_df["company_name"].notna()]
+
 # Remove columns with all na entries
 remove_features = [
     "direct_availability_proportion",
@@ -17,10 +23,18 @@ remove_features = [
     "sublet_availability_proportion",
     "sublet_internal_class_rent",
     "sublet_overall_rent",
-    "leases"
+    "leasing",
+    "space_type",
+    "RBA"
 ]
 for feature in remove_features:
     leases_df = leases_df[leases_df[feature].notna()]
 
 # Export
 leases_df.to_csv("../data/cleaned_leases.csv", index=False)
+
+# Extract RBA
+leases_by_rba = leases_df["RBA"]
+leases_by_rba.head()
+
+leases_by_rba.to_csv("../data/leases_by_rba.csv")
